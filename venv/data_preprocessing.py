2b994ed0ba31cbfa12b836e14583aaad50b25137
Blak3Nick <blaken2021@gmail.com>
2018-11-23 05:20:57 -0500

    working with the data

--- venv/data_preprocessing.py
+++ venv/data_preprocessing.py
@@ -2,8 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing the data
 df = dataset = pd.read_csv('Data.csv')
-print(df)
+#print(df)
 
 X = dataset.iloc[:, :-1].values
+#print(X)
+
+#missing data solution
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 print(X)
