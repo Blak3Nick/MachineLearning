7e6ec850ed30e5758c71c7205cc109d8cf74a877
Blak3Nick <blaken2021@gmail.com>
2018-11-24 06:35:50 -0500

    working with the data

--- venv/mulitple_linear_regression.py
+++ venv/mulitple_linear_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
