cbdd643321aea693d03ce6148cf88d26183265fe
Blak3Nick <blaken2021@gmail.com>
2018-11-25 05:28:30 -0500

    working with the data

--- venv/mulitple_linear_regression.py
+++ venv/mulitple_linear_regression.py
@@ -8,11 +8,9 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+y = dataset.iloc[:, 4].values
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -32,9 +30,14 @@ X = onehotencoder.fit_transform(X).toarray()
 #Avoiding the Dummy variable trap-remove one of the dummy variables
 """most libraries do this automatically"""
 X = X[:, 1:]
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 #Fitting the Multiple Linear Regression t othe Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 #Predicting the Test set results
+Y_pred = regressor.predict(X_test)
