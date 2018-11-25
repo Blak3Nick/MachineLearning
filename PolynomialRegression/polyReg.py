eafee63689f0eb0102c85c72dd54d5296d698af1
Blak3Nick <blaken2021@gmail.com>
2018-11-25 07:02:58 -0500

    working with the data

--- PolynomialRegression/polyReg.py
+++ PolynomialRegression/polyReg.py
@@ -2,3 +2,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+#encoding the categories
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3]= labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
