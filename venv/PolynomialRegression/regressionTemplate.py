5c38fd1f66c63827f77318b249ce4019038bee7d
Blak3Nick <blaken2021@gmail.com>
2018-11-26 13:34:33 -0500

    working with the data

--- venv/PolynomialRegression/regressionTemplate.py
+++ venv/PolynomialRegression/regressionTemplate.py
@@ -10,12 +10,14 @@ X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+"""
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+#sc_y = StandardScaler()
+#y_train = sc_y.fit_transform(y_train)
+"""
 
 #encoding the categories
 # from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -42,16 +44,19 @@ print(lin_reg.predict([[6.5]]))
 
 #predict poly
 print(lin_reg2.predict(poly_reg.fit_transform([[6.5]])))
-
+y_pred = regressor.predict(6.5)
 #Visualize the  linear results
-plt.scatter(X, y, color='red')
-plt.plot(X, lin_reg.predict(X), color='green')
-plt.title('Linear Regression')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
+""""
+#plt.scatter(X, y, color='red')
+# plt.plot(X, lin_reg.predict(X), color='green')
+# plt.title('Linear Regression')
+# plt.xlabel('Position level')
+# plt.ylabel('Salary')
+# plt.show()
+"""
 
 #Visualize the polynomial results
+#for higher resolution include the next two lines
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
