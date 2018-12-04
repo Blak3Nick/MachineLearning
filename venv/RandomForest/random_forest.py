1ff93a0ffb34546ed4a9b6c045fbd0b4425f2a3a
Blak3Nick <blaken2021@gmail.com>
2018-12-04 16:22:30 -0500

    working with the data

--- venv/RandomForest/random_forest.py
+++ venv/RandomForest/random_forest.py
@@ -25,35 +25,11 @@ y = dataset.iloc[:, 2].values
 # X[:, 3]= labelencoder_X.fit_transform(X[:, 3])
 # onehotencoder = OneHotEncoder(categorical_features=[3])
 # X = onehotencoder.fit_transform(X).toarray()
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor()
 
-#Fitting the linear regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
-
-#Fitting polynomial regression
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
-lin_reg2 = LinearRegression()
-lin_reg2.fit(X_poly, y)
-
-#Make linear predictions
-
-print(lin_reg.predict([[6.5]]))
-
-#predict poly
-print(lin_reg2.predict(poly_reg.fit_transform([[6.5]])))
 y_pred = regressor.predict(6.5)
-#Visualize the  linear results
-""""
-#plt.scatter(X, y, color='red')
-# plt.plot(X, lin_reg.predict(X), color='green')
-# plt.title('Linear Regression')
-# plt.xlabel('Position level')
-# plt.ylabel('Salary')
-# plt.show()
-"""
+
 
 #Visualize the polynomial results
 #for higher resolution include the next two lines
@@ -61,7 +37,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='red')
 plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color='green')
-plt.title('Polynomial Regression')
+plt.title('Random Forest')
 plt.xlabel('Position level with .1 steps')
 plt.ylabel('Salary')
 plt.show()
